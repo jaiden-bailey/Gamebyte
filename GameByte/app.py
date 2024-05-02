d7a157271781ff106152157b32d7c14b399128f7
Jaiden Bailey <107886395+jaiden-bailey@users.noreply.github.com>
2024-05-01 22:30:11 -0400

    Updated app.py
    Added routes for weather and get-weather. Added the api data and the function.

--- GameByte/app.py
+++ GameByte/app.py
@@ -9,6 +9,10 @@ import requests
 import datetime as dt
 
 
+def K_to_C_F(K):
+    C = K - 273.15
+    F = C * (9/5) + 32
+    return C, F
 
 app = Flask(__name__)
 
@@ -22,8 +26,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-app.config['SESSION_PERMANENT'] = False
-
 users = {'mike': {'password': 'milk_123'}}
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site123.db' # Local SQLite database
@@ -34,6 +36,8 @@ db = SQLAlchemy(app)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+app.config['SESSION_PERMANENT'] = False
 
 
 # Define the User model
@@ -107,6 +111,43 @@ def register():
     else:
         return render_template('register.html')
 
+@app.route('/get-weather', methods=['POST'])
+
+def weather_api():
+    City = request.form['city']
+    baseurl = "http://api.openweathermap.org/data/2.5/weather?"
+
+    with open("api_key.txt", "r") as f:
+        for line in f.readlines():
+            API_key = line.rstrip()
+
+    
+    url = baseurl + "appid=" + API_key + "&q=" + City
+
+    response = requests.get(url).json()
+
+
+    temp_k = response['main']['temp']
+    temp_c, temp_f = K_to_C_F(temp_k)
+    feels_like_K = response['main']['feels_like']
+    feels_like_C, feels_like_F = K_to_C_F(feels_like_K)
+    wind_speed = response['wind']['speed']
+    humidity = response['main']['humidity']
+    description = response['weather'][0]['description']
+    sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+    sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+    print(f"tempeture in {City}: {temp_c:.2f}°C or {temp_f:.2f}°F")
+    print(f"tempeture in {City} feels like: {feels_like_C:.2f}°C or {feels_like_F:.2f}°F")
+    print(f"humidity in {City}: {humidity}%")
+    print(f"wind speed in {City}: {wind_speed}m/s")
+    print(f"general weather in {City}: {description}")
+    print(f"sun rises in {City}: {sunrise_time}")
+    print(f"sun sets in {City}: {sunset_time}")
+
+
+    return render_template('get_weather.html', temp_c=temp_c, temp_f=temp_f, feels_like_C=feels_like_C, feels_like_F=feels_like_F, humidity=humidity, wind_speed=wind_speed, description=description, sunrise_time=sunrise_time, sunset_time=sunset_time, City=City)
+
 
 
 # Logout route
@@ -116,6 +157,10 @@ def logout():
     logout_user()
     session.clear() 
     return render_template('index.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
 
 
 if __name__ == '__main__':
